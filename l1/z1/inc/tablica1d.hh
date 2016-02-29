d08ca9f375fafafa040dabad3c370674d9eb8a30
218665 <218665@student.pwr.edu.pl>
2016-02-29 10:15:39 +0100

    10:14

--- l1/z1/inc/tablica1d.hh
+++ l1/z1/inc/tablica1d.hh
@@ -7,8 +7,9 @@ class tablica1D
 private:
   int* tablica_ptr;
   int rozmiar=10;
+  int index=0;
 public:
-  tablica1D()  {tablica_ptr = new int [rozmiar];  for(int i=0 ; i<rozmiar; i++) tablica_ptr[i] = std::rand()%MAX ;} //konstruktor
+  tablica1D()  {tablica_ptr = new int [10];} //konstruktor
   ~tablica1D() {delete [] tablica_ptr;} //destruktor
   void Wypisz(); //Wypisywanie elementów tablicy
   void DodajElement(int Element);
--- l1/z1/src/tablica1d.cpp
+++ l1/z1/src/tablica1d.cpp
@@ -9,7 +9,16 @@ void tablica1D::Wypisz()
   cout << endl;
 }
 
-void tablica1d::DodajElement(int Element)
+void tablica1D::DodajElement(int Element)
 {
-  
+  int *tablica_pom;
+  if(index<rozmiar) tablica_ptr[++index]=Element;
+  else {
+    tablica_pom = new int [rozmiar];
+    for (int i=0; i<rozmiar; ++i) tablica_pom[i]=tablica_ptr[i]; // spróbować konstruktor kopiujący
+    delete [] tablica_ptr;
+    tablica_ptr = new int [++rozmiar];
+    for (int i=0; i<rozmiar; ++i) tablica_ptr[i]=tablica_pom[i];
+    tablica_ptr[++index]=Element;
+  }
 }
