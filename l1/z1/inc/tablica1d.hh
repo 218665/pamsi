3ec9bc57e9c9ce4dcf414b58f8ca8bd7f79dd673
Dawid Marszalkiewicz <leh.16.dm@gmail.com>
2016-03-04 20:33:10 +0100

    Bez wycieku pamieci

--- l1/z1/inc/tablica1d.hh
+++ l1/z1/inc/tablica1d.hh
@@ -1,15 +1,15 @@
 #ifndef TABLICA1D
 #define TABLICA1D
 #include <cstdlib>
-#define MAX 100
+
 class tablica1D
 {
 private:
   int* tablica_ptr;
-  int rozmiar=10;
-  int index=-1;
+  int rozmiar = 10;
+  int index = -1;
 public:
-  tablica1D()  {tablica_ptr = new int [10];} //konstruktor
+  tablica1D()  {tablica_ptr = new int [rozmiar];} //konstruktor
   ~tablica1D() {delete [] tablica_ptr;} //destruktor
   void Wypisz(); //Wypisywanie elementów tablicy
   void DodajElement(int Element);
--- l1/z1/src/main.cpp
+++ l1/z1/src/main.cpp
@@ -3,13 +3,23 @@
 #include <cstdlib>
 #define MAXRAND 100
 using namespace std;
-#define ROZMIAR_TABLICY 1000000
+#define ROZMIAR_TABLICY 100000
+
+double obliczSekundy( clock_t czas )
+{
+    return static_cast < double >( czas ) / CLOCKS_PER_SEC;
+}
+
+
 int main()
 {
   tablica1D Ex;
-  for(int i=0; i<ROZMIAR_TABLICY; ++i)
-    {
+  for(int i=0; i<ROZMIAR_TABLICY; ++i){
       Ex.DodajElement(rand() %MAXRAND);
-      Ex.Wypisz();
-    }
+      cout<< i <<endl;
+  }
+  
+  cout << "Liczba sekund to:"<<obliczSekundy(clock())<< endl;
+    
 }
+
